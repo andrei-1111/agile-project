3beca94addd1243293ec4b7e542d9ff1920d07d5
Andrei Elizaga <naaeliz@gmail.com>
2020-05-15 05:00:05 +0800

    Add test viewsets for Agile model.
    ticket #4

--- agile_project/main/tests/test_viewsets.py
+++ agile_project/main/tests/test_viewsets.py
@@ -20,7 +20,7 @@ def user():
 
 @pytest.fixture
 def agile_value():
-    return Value.objects.create(
+    return Agile.objects.create(
         name='Individuals and interactions over processes and tools',
         description='This value of the Agile manifesto focuses on giving importance',
     )
@@ -43,12 +43,94 @@ def logged_in_client(user):
 
 
 @pytest.mark.django_db
+class TestAgileValueViews:
+    @pytest.fixture
+    def defaults(self):
+        payload = {
+            'name': 'Working product over comprehensive documentation',
+            'description': 'The Agile values dictate that the first and foremost duty',
+        }
+        return {
+            'payload': payload,
+        }
+
+    def get_detail_url(self, value_id):
+        return reverse('agile:agile-detail', args=[value_id])
+
+    def test_unauthenticated_list_view(self, client):
+        """Tests get request are allowed for unauthenticated Users."""
+        url = reverse('agile:agile-list')
+        resp = client.get(url)
+        assert resp.status_code == status.HTTP_200_OK
+
+    def test_list_view(self, logged_in_client):
+        url = reverse('agile:agile-list')
+        resp = logged_in_client.get(url)
+        assert resp.status_code == status.HTTP_200_OK
+
+    def test_get_view(self, logged_in_client, agile_value):
+        url = self.get_detail_url(agile_value.id)
+        resp = logged_in_client.get(url)
+        assert resp.status_code == status.HTTP_200_OK
+
+    def test_unauthenticated_get_view(self, client, agile_value):
+        """Tests get request are allowed for unauthenticated Users."""
+        url = self.get_detail_url(agile_value.id)
+        resp = client.get(url)
+        assert resp.status_code == status.HTTP_200_OK
+
+    def test_unauthenticated_post(self, client, defaults):
+        """Tests post request creates a Agile instance."""
+        url = reverse('agile:agile-list')
+        resp = client.post(url, defaults['payload'])
+        assert resp.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_post(self, logged_in_client, defaults):
+        """Tests post request creates a Value instance."""
+        url = reverse('agile:agile-list')
+        resp = logged_in_client.post(url, defaults['payload'])
+        assert resp.status_code == status.HTTP_201_CREATED
+        assert Agile.objects.filter(id=resp.json()['id']).exists() == True
+
+    def test_unauthenticated_patch(self, client, agile_value, defaults):
+        """Tests post request creates a Agile instance."""
+        url = self.get_detail_url(agile_value.id)
+        resp = client.patch(url, defaults['payload'])
+        assert resp.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_patch(self, logged_in_client, agile_value):
+        """Tests patch request updates the instance."""
+        url = self.get_detail_url(agile_value.id)
+        payload = {'description': 'New Description'}
+        resp = logged_in_client.patch(url, payload)
+        assert resp.status_code == status.HTTP_200_OK
+        agile_value = Agile.objects.get(id=resp.json()['id'])
+        assert agile_value.description == payload['description']
+
+    def test_unauthenticated_put(self, client, agile_value, defaults):
+        """Tests post request creates a Agile instance."""
+        url = self.get_detail_url(agile_value.id)
+        resp = client.put(url, defaults['payload'])
+        assert resp.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_put(self, logged_in_client, agile_value):
+        """Tests put request updates an instance."""
+        url = self.get_detail_url(agile_value.id)
+        payload = {'name': 'New Name', 'description': 'New Description'}
+        resp = logged_in_client.put(url, payload)
+        assert resp.status_code == status.HTTP_200_OK
+        agile_value = Agile.objects.get(id=resp.json()['id'])
+        assert agile_value.description == payload['description']
+        assert agile_value.name == payload['name']
+
+
+@pytest.mark.django_db
 class TestPrincipleViews:
     @pytest.fixture
     def defaults(self):
         payload = {
-            'name': 'name',
-            'description': 'description',
+            'name': 'Customer satisfaction through early and continuous software delivery',
+            'description': 'Customers are happier when they receive working software',
         }
         return {
             'payload': payload,
