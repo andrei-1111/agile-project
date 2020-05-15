9bdfde0ebf5b9c4227a77f62cbeb2aecfdd6db7b
Andrei Elizaga <naaeliz@gmail.com>
2020-05-15 18:05:44 +0800

    Update Agile model useage on serializers and viewsets.

--- agile_project/main/serializers.py
+++ agile_project/main/serializers.py
@@ -2,29 +2,17 @@ from rest_framework import serializers
 
 from .models import (
     Agile,
-    Principle,
 )
 
 
-class AgileValueSerializer(serializers.ModelSerializer):
+class AgileSerializer(serializers.ModelSerializer):
     """Serializer for Agile model."""
 
     class Meta:
         model = Agile
         fields = (
             'id',
-            'name',
-            'description',
-        )
-
-
-class AgilePrincipleSerializer(serializers.ModelSerializer):
-    """Serializer for Principle model."""
-
-    class Meta:
-        model = Principle
-        fields = (
-            'id',
+            'type',
             'name',
             'description',
         )
--- agile_project/main/tests/test_serializers.py
+++ agile_project/main/tests/test_serializers.py
@@ -6,11 +6,9 @@ from typing import Dict
 
 from ..models import (
     Agile,
-    Principle,
 )
 from ..serializers import (
-    AgileValueSerializer,
-    AgilePrincipleSerializer,
+    AgileSerializer,
 )
 
 
@@ -32,40 +30,11 @@ class TestAgileSerializer:
         }
 
     def test_valid(self, defaults):
-        serializer = AgileValueSerializer(data=defaults['data'])
+        serializer = AgileSerializer(data=defaults['data'])
         assert serializer.is_valid() == True
 
     def test_save(self, defaults):
-        serializer = AgileValueSerializer(data=defaults['data'])
-        serializer.is_valid()
-        instance = serializer.save()
-        assert instance.name == defaults['data']['name']
-        assert instance.description == defaults['data']['description']
-
-
-@pytest.mark.django_db
-class TestAgilePrincipleSerializer:
-    @pytest.fixture
-    def agile_fixture(self, db) -> Principle:
-        return Principle.objects.create(name='name', description='description')
-
-    @pytest.fixture
-    def defaults(self, agile_fixture) -> Dict[str, Dict[str, str]]:
-        data = {
-            'name': 'Frequent delivery of working software',
-            'description': 'Scrum accommodates this principle',
-        }
-        return {
-            'page': agile_fixture,
-            'data': data,
-        }
-
-    def test_valid(self, defaults):
-        serializer = AgilePrincipleSerializer(data=defaults['data'])
-        assert serializer.is_valid() == True
-
-    def test_save(self, defaults):
-        serializer = AgilePrincipleSerializer(data=defaults['data'])
+        serializer = AgileSerializer(data=defaults['data'])
         serializer.is_valid()
         instance = serializer.save()
         assert instance.name == defaults['data']['name']
